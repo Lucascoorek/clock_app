2bf17fbe680e5151ea32458330e04e9ac538c09f
Lukasz Kurek <lukascoorek@gmail.com>
2019-04-09 21:05:58 +0100

    three generators

--- src/script.js
+++ src/script.js
@@ -1,30 +1,46 @@
 export default function() {
-  const { body } = document;
-
   const div = document.querySelector(".clock");
   const currentTime = new Date();
   let seconds = currentTime.getSeconds();
   let minutes = currentTime.getMinutes();
   let hours = currentTime.getHours();
-
   hours = hours > 12 ? hours - 12 : hours;
+
   function* secondsGen() {
-    yield seconds++;
+    while (true) {
+      yield seconds++;
+    }
   }
+  const secGen = secondsGen();
+
+  function* minutesGen() {
+    while (true) {
+      yield minutes++;
+    }
+  }
+  const minGen = minutesGen();
+
+  function* hoursGen() {
+    while (true) {
+      yield hours++;
+    }
+  }
+  const hGen = hoursGen();
+
   setInterval(function() {
-    const gen = secondsGen();
-    gen.next().value;
+    secGen.next().value;
     if (seconds === 60) {
-      minutes++;
-      if (minutes === 60) {
-        minutes = 0;
-        hours++;
-        if (hours === 12) {
-          hours = 0;
-        }
-      }
+      minGen.next().value;
       seconds = 0;
     }
+    if (minutes === 60) {
+      hGen.next().value;
+      minutes = 0;
+    }
+    if (hours === 12) {
+      hours = 0;
+    }
+
     div.innerHTML = `${hours
       .toString()
       .padStart(2, "0")}:${minutes
